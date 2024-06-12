e099e01af4540426ee43bcb3b12be2b55456860a
usmankhan18 <usman.khan@ovrlod.com>
2024-06-12 15:17:58 +0500

    Update Product.cs

--- InventoryManagement.Domain/Models/Product.cs
+++ InventoryManagement.Domain/Models/Product.cs
@@ -3,10 +3,10 @@
     public class Product
     {
         public int Id { get; set; }
-        public int ManufacturerId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        public int? ManufacturerId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
         public string? LogoPath { get; set; }
     }
 }
